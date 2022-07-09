f16a58d1f907595a2026fc310a1957848728d7d0
Katharine Swilley <kswilley@gmail.com>
2022-07-09 16:55:06 -0700

    resubmit for lab17

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # lab17-aws-s3-lambda
+
+## Katharine Swilley
+
+### Problem Domain
+
+AWS Lambda allows writing code that is triggered in the cloud, without thinking about maintaining servers. We’ll use it today to automatically run some processing on image files after they’re uploaded to an S3 Bucket
+
+- Create an S3 Bucket with “open” read permissions, so that anyone can see the images/files in their browser
+- A user should be able to upload an image at any size, and update a dictionary of all images that have been uploaded so far
+- When an image is uploaded to your S3 bucket, it should trigger a Lambda function which must:
+  - Download a file called “images.json” from the S3 Bucket if it exists
+  - The images.json should be an array of objects, each representing an image. Create an empty array if this file is not present
+  - Create a metadata object describing the image: Name, Size, Type, etc.
+  - Append the data for this image to the array: If the image is a duplicate name, update the object in the array, don’t just add it
+  - Upload the images.json file back to the S3 bucket
+
+### Links and Resources
+
+[images.json](https://image-bucket-lab17.s3.us-west-2.amazonaws.com/images.json)
+
+### Setup
+
+- Lambda:  `lab17-image-upload` pulls file information from  jpg image files added to the `image-bucket-lab17` bucket. file information is stored in the `images.json` file
--- index.js
+++ index.js
@@ -1,30 +1,54 @@
-// Initial commit for lab 17
-
+// Updated resubmit for lab 17 - completed after code review
 const AWS = require('aws-sdk');
-
 const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
-    // TODO implement
-   
-//   let bucketName = 'lab17-image-bucket';
-    if(!'image.json') {
+
+    let bucketName = event.Records[0].s3.bucket.name;
+    let fileName = event.Records[0].s3.object.key;
+    let fileSize = event.Records[0].s3.object.size;
         
-        let bucketName = event.Records[0].s3.bucket.name;
-        let key = event.Records[0].s3.object.key;
-        
-        let imageObj = await s3.getObject({Bucket: bucketName, Key: key}).promise();
-        let imageData = JSON.parse(imageObj.Body.toString());
+   console.log('event: ', bucketName, fileName, fileSize);
    
-        let metaData = {
-          name: fileName,
-          size: fileSize,
-          type: 'image'
-        }
-        imageData.push(metaData);
-        
-    }
+   const params = {
+       Bucket: bucketName,
+       Key: 'images.json',
+   }
    
+   try {
+       const imageManifest = await s3.getObject(params).promise();
+       
+       let manifestData = JSON.parse(imageManifest.Body.toString());
+       console.log('Did this work? Did we get manigestData?', manifestData);
+       
+       manifestData.push({
+            name: fileName,
+            size: fileSize,
+            type: 'image'
+       });
+       
+       let manifestBody = JSON.stringify(manifestData);
+          
+       const newManifest = await  s3.putObject({...params, Body: manifestBody,
+       ContentType: 'application/json'}).promise();
+       
+       console.log('we got here');
+       
+   } catch(e) {
+      console.log(e)
+      
+      if(e.message === 'The specified key does not exist.'){
+          const newManifest = {
+              Bucket: bucketName,
+              Key: 'images.json',
+              Body: JSON.stringify([{name: fileName, size: fileSize, type: 'image/jpg'}]),
+              ContentType: 'application/json',
+          };
+          const manifest = await s3.putObject(newManifest).promise();
+          console.log('JSON file created for bucket', manifest);
+      }
+      console.log('need to create images.json'); 
+   }
    
     const response = {
         statusCode: 200,
